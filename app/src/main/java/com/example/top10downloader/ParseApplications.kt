65eb2962fe9927e87c129c073d9f92d700c71089
Bojan Antic <b.antic@levi9.com>
2019-11-28 23:20:45 +0100

    -- added some logic

---
+++ app/src/main/java/com/example/top10downloader/ParseApplications.kt
@@ -0,0 +1,68 @@
+package com.example.top10downloader
+
+import android.util.Log
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
+
+class ParseApplications {
+
+    private val TAG = "ParseApplication"
+    var applications = ArrayList<FeedEntry>()
+
+    fun parse(xmlData: String): Boolean {
+        Log.d(TAG, "parse() called with $xmlData")
+        var status = true
+        var inEntry = false
+        var textValue = ""
+        try {
+            val factory = XmlPullParserFactory.newInstance()
+            factory.isNamespaceAware = true
+            val xpp = factory.newPullParser()
+            xpp.setInput(xmlData.reader())
+            var eventType = xpp.eventType
+            var currentRecord = FeedEntry()
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                val tagName = xpp.name?.toLowerCase()
+                when (eventType) {
+                    XmlPullParser.START_TAG -> {
+                        Log.d(TAG, "Parse: starting tag for $tagName")
+                        if (tagName == "entry") {
+                            inEntry = true
+                        }
+                    }
+
+                    XmlPullParser.TEXT -> textValue = xpp.text
+
+                    XmlPullParser.END_TAG -> {
+                        Log.d(TAG, "Parse: ending tag for $tagName")
+                        if (inEntry) {
+                            when (tagName) {
+                                "entry" -> {
+                                    applications.add(currentRecord)
+                                    inEntry = false
+                                    currentRecord = FeedEntry() //Creating new object
+                                }
+                                "name" -> currentRecord.name = textValue
+                                "artist" -> currentRecord.artist = textValue
+                                "releasedate" -> currentRecord.releaseDate = textValue
+                                "summery" -> currentRecord.summery = textValue
+                                "image" -> currentRecord.imageUrl = textValue
+                            }
+                        }
+                    }
+                }
+                eventType = xpp.next()
+            }
+
+            for (app in applications){
+                Log.d(TAG, "********************")
+                Log.d(TAG, app.toString())
+            }
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            status = false
+        }
+        return status
+    }
+}
